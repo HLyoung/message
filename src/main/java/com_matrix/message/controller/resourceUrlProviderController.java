824f2e316bc9b850f9c833eaec29a46837c04f6b
HLyoung <1017348166@qq.com>
2017-10-19 20:56:06 +0800

    ...

---
+++ src/main/java/com_matrix/message/controller/resourceUrlProviderController.java
@@ -0,0 +1,24 @@
+
+
+package com_matrix.message.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.resource.ResourceUrlProvider;
+
+/**
+ * 处理静态资源URL
+ *
+ */
+@ControllerAdvice
+public class resourceUrlProviderController {
+
+    @Autowired
+    private ResourceUrlProvider resourceUrlProvider;
+
+    @ModelAttribute("urls")
+    public ResourceUrlProvider urls() {
+        return this.resourceUrlProvider;
+    }
+}
--- src/main/webapp/WEB-INF/jsp/page/page2.jsp
+++ src/main/webapp/WEB-INF/jsp/page/page2.jsp
@@ -2,7 +2,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-<title>Spring Boot Sample</title>
+	<title>Spring Boot Sample</title>
+	<script type="text/javascript" src="${pageContext.request.contextPath}${urls.getForLookupPath('/jquery/3.2.1/jquery-3.2.1.js')}"></script>
 </head>
 
 <body>
